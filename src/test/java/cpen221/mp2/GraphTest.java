0c3c24b00406b73d95f1a076912008376c381adf
DantePrins <dante.prins@alumni.ubc.ca>
2019-10-20 12:37:57 -0700

    done testing, does not currently cover exceptions or assertions, will cover once i get a response on campus wire

--- src/test/java/cpen221/mp2/GraphTest.java
+++ src/test/java/cpen221/mp2/GraphTest.java
@@ -5,10 +5,7 @@ import cpen221.mp2.graph.Graph;
 import cpen221.mp2.graph.Vertex;
 import org.junit.Test;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 import static org.junit.Assert.*;
 
@@ -37,25 +34,36 @@ public class GraphTest {
         assertEquals(e2, g.getEdge(v2, v3));
         assertEquals(21, g.shortestPath(v2, v2));
     }
+
     @Test
-    public void notFinal(){
+    public void mutator1(){
         List<Vertex> test= new ArrayList<>();
         Vertex tester= new Vertex(1,"hello");
         test.add(tester);
         tester=new Vertex(2,"bye");
         assert(test.get(0).id()==1);
     }
+
     @Test
-    public void notFinal2(){
-        List<Edge> test= new ArrayList<>();
-        Edge tester= new Edge(new Vertex(1,""),new Vertex(2,""),20);
-        test.add(tester);
-        assert(test.contains(new Edge(new Vertex(1,""),new Vertex(2,""),10)));
+    public void mutator2(){
+        Vertex v1 = new Vertex(1, "A");
+        Vertex v2 = new Vertex(2, "B");
+        Vertex v3 = new Vertex(3, "C");
+        Edge<Vertex> e1 = new Edge<>(v1, v2, 5);
+        Graph<Vertex, Edge<Vertex>> g = new Graph<>();
+        g.addVertex(v1);
+        g.addVertex(v2);
+        g.addVertex(v3);
+        g.addEdge(e1);
+        e1=new Edge<>(v1,v3, 3);
+        assertFalse(g.edge(v1,v3));
+        assertTrue(g.edge(v1,v2));
 
 
     }
+    
     @Test
-    public void graphBasic(){
+    public void graphBasic1(){//coverage from repInv to allEdges
         Vertex v1 = new Vertex(1, "A");
         Vertex v2 = new Vertex(2, "B");
         Vertex v3 = new Vertex(3, "C");
@@ -92,11 +100,87 @@ public class GraphTest {
         g.addEdge(e1);
         g.addEdge(e2);
         g.addEdge(e3);
-        e1=new Edge<>(v3,v1,2);
+
+
         assertEquals(g.edgeLength(v1,v4),9);
+        assertEquals(g.edgeLength(v1,v3),Integer.MAX_VALUE);
+
+        assertEquals(g.edgeLengthSum(),21);
+
+        assertTrue(g.remove(e3));
+        assertEquals(g.edgeLengthSum(),12);
+        assertFalse(g.remove(e3));
+
+        Vertex v5 = new Vertex(5, "E");
+        g.addVertex(v5);
+
+        assertTrue(g.remove(v5));
+        assertFalse(g.remove(v5));
+        Set<Vertex> allVer= new HashSet<>();
+        allVer.add(v1);
+        allVer.add(v2);
+        allVer.add(v4);
+        assertFalse(g.allVertices().equals(allVer));
+
+        allVer.add(v3);
+        assertTrue(g.allVertices().equals(allVer));
+
+        Set<Vertex> mutator =g.allVertices();
+        mutator.remove(v3);
+        assertTrue(g.allVertices().equals(allVer));
+
+        Set<Edge<Vertex>> allEdg= new HashSet<>();
+        allEdg.add(e1);
+        allEdg.add(e2);
+        assertTrue(allEdg.equals(g.allEdges()));
+        allEdg.add(e3);
+        assertNotEquals(allEdg,g.allEdges());
 
     }
 
+    @Test
+    public void graphBasic2() {//coverage from getNeighbors to GetEdge
+
+        Vertex v1 = new Vertex(1, "A");
+        Vertex v2 = new Vertex(2, "B");
+        Vertex v3 = new Vertex(3, "C");
+        Vertex v4 = new Vertex(4, "D");
+
+        Edge<Vertex> e1 = new Edge<>(v2, v1, 5);
+        Edge<Vertex> e2 = new Edge<>(v2, v3, 7);
+        Edge<Vertex> e3 = new Edge<>(v1, v4, 9);
+
+        Graph<Vertex, Edge<Vertex>> g = new Graph<>();
+        g.addVertex(v1);
+        g.addVertex(v2);
+        g.addVertex(v3);
+        g.addVertex(v4);
+        g.addEdge(e1);
+        g.addEdge(e2);
+        g.addEdge(e3);
+        Map<Vertex,Edge<Vertex>> neigh= new HashMap<>();
+        neigh.put(v4,e3);
+        neigh.put(v2,e1);
+        assertTrue(g.getNeighbours(v1).equals(neigh));
+        assertTrue(g.getEdge(v1,v2).equals(e1));
+
+        List<Vertex> path=new ArrayList<>();
+        path.add(v1);
+        path.add(v2);
+        path.add(v3);
+        assertEquals(g.pathLength(path),12);
+        path.add(v4);
+        assertEquals(g.pathLength(path),Integer.MAX_VALUE);
+
+
+
+
+
+        }
+        // @test()
+    public void except1(){
+
+    }
 
 
 
